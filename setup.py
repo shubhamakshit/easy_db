58efa29f9b57172e67581f783498088e7fe27b48
Akshit shubham <121078566+shubhamakshit@users.noreply.github.com>
2024-12-31 21:11:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='easy_db',
+    name='easy_mongo',
     version='0.1.1',
     author='Shubham Akshit',
     author_email='akshitshubhammas@gmail.com',
